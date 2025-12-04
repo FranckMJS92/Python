b6e9cb04d1bc8b3ebad71400beabdf11762f59b4
Franck <francklopez92@outlook.es>
2025-12-04 20:25:42 +0100

    Updates clases Python

--- README.md
+++
@@ -1,2 +0,0 @@
-# Python
-Clases de Python
--- UD1/Utilidades.py
+++
@@ -1,8 +0,0 @@
-def pedir_entero(mensaje):
-    while True:
-        try:
-            valor = int(input(mensaje))
-        except ValueError:
-            print("El valor ingresado no es un numero")
-
-valor1 = pedir_entero("Dime tu edad: ")
