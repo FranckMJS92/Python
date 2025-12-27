ac923a3e0b909bf3b9a14ea07fa058de2bb7bfa8
Franck <francklopez92@outlook.es>
2025-12-27 19:12:56 +0100

    Update Entregable 2 - Parte1 y Utilidades

--- Entregable2/Ejercicio1.py
+++ Entregable2/Ejercicio1.py
@@ -1,9 +1,48 @@
 import Utilidades
+import random
 
-count = 0
-while count < 3:
-    print("========= JUEGO PIEDRA PAPEL O TIJERA =========")
-    workers = Utilidades.pedir_entero(
+wins = 0
+winUser = 0
+winMachine = 0
+while wins < 3:
+    print("\n========= JUEGO PIEDRA PAPEL O TIJERA =========")
+    # Introduce el usuario su opcion
+    userOption = Utilidades.pedir_entero_rango(
         "Introduce tu jugada (1-Piedra | 2-PApel | 3-Tijera) : "
     )
-    count += 1
+
+    # Se genera la opcion de la maquina
+    machineOption = random.randint(1, 3)
+
+    # Variables para guardar las opciones
+    gameUser = Utilidades.game(userOption)
+    gameMachine = Utilidades.game(machineOption)
+
+    print(f"Tu jugada : {gameUser}")
+    print(f"Jugada de la máquina : {gameMachine}")
+
+    # Variable result almacena: string -> mensaje de cada ronda | int -> para evaluar las wins totales
+    result = Utilidades.resultString(userOption, machineOption)
+    print(f"Resultado : {result[0]}")
+
+    # Logica para sumar las wins de cada partida
+    # Los empates no suman por lo que de haber empates se sigue jugando hasta que hayan 3 victorias indistintamente del jugador
+    match result[1]:
+        case 0:
+            winMachine += 1
+            # break
+        case 1:
+            wins += 0
+            # break
+        case 2:
+            winUser += 1
+            # break
+
+    wins = winUser + winMachine
+
+print("\n=========================================")
+if winUser < winMachine:
+    print(f"La maquina gana la partida final : {winUser} - {winMachine}")
+else:
+    print(f"Has ganado la partida final : {winUser} - {winMachine}")
+print("=========================================")
--- Entregable2/Utilidades.py
+++ Entregable2/Utilidades.py
@@ -1,17 +1,7 @@
 import math
 
-# ARCHIVO DE UTILIDADES PARA EJERCICIOS 1 Y 2
-
-def pedir_entero(mensaje):
-    while True:
-        try:
-            valor = int(input(mensaje))
-            return valor
-        except ValueError:
-            print("El valor no es entero")
-
-
-def pedir_entero_rango(mensaje, min=0, max=1):
+# Valida la opcion ingresada por usuario en el rango de 1 a 3
+def pedir_entero_rango(mensaje, min=1, max=3):
     while True:
         try:
             valor = int(input(mensaje))
@@ -22,5 +12,23 @@ def pedir_entero_rango(mensaje, min=0, max=1):
         except ValueError:
             print("El valor no es entero")
 
-def redondear(numero):
-    return round(numero, 2)
+# Define la interpretacion de la opcion ingresada por el usuario
+def game(numero):
+    match numero:
+        case 1:
+            return "Piedra"
+        case 2:
+            return "Papel"
+        case 3:
+            return "Tijera"
+
+# Valida el resultado y devuelve un String y un entero
+# El string se muestra directo en el resultado de cada ronda
+# El entero sirve para evaluar la cantidad de victorias y mostrar el resultado final
+def resultString(n1, n2):
+    if n1 < n2:
+        return "Pierdes esta ronda ... ",0
+    elif n1 == n2:
+        return "Empates ...",1
+    elif n1 > n2:
+        return "¡Ganas esta ronda!",2
