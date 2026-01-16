f6e76935a22d3d54b19fd13ea7c93b8951722785
Franck <francklopez92@outlook.es>
2026-01-16 20:31:26 +0100

    Update Friday

--- UD_4_Actividad_1_Map_Filter/EJercicio10.py
+++ UD_4_Actividad_1_Map_Filter/EJercicio10.py
@@ -1,6 +1,10 @@
 # EJERCICIO 10
 
+
+
 print("EJERCICIO 10")
+
+
 
 # Funcion Menu
 def menu():
@@ -12,11 +16,15 @@ def menu():
     print("4. Salir del programa.")
     input("Dime una opcion : ")
 
+
+
 # Funcion Aplicar IVA
 def aplicarIVA():
     print(productos.items)
     print(productos.values)
     # precioConIVA= map(lambda item : )
+
+
 
 # Diccionario de productos
 productos = {
@@ -26,6 +34,8 @@ productos = {
     "456789": {"nombre": "Huevos", "precio": 2.5, "stock": 12, "categoria": "Lácteo"},
     "567890": {"nombre": "Platanos", "precio": 1.8, "stock": 3, "categoria": "Fruta"},
 }
+
+
 
 menu()
 aplicarIVA()
