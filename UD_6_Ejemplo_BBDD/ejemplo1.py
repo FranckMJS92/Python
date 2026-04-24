135c37c870b7b38f9443192a5a79cfbeb746ff2d
Franck <francklopez92@outlook.es>
2026-04-24 13:23:27 +0200

    Clase BBDD
    Actualizacion ejemplo base de datos

--- UD_6_Ejemplo_BBDD/ejemplo1.py
+++ UD_6_Ejemplo_BBDD/ejemplo1.py
@@ -17,6 +17,7 @@ finally:
 # OObtenemos el cursos para poder ejecutar consultas
 cursor = conexion.cursor()
 
+"""
 sentenciaInsert = "INSERT INTO empleados (nombre, apellido, departamento, puesto, salario) VALUES (%s,%s,%s,%s,%s)"
 valoresInsert = ("Paco","Perez","Desarrollo","Developer","12345")
 cursor.execute(sentenciaInsert, valoresInsert)
@@ -27,3 +28,47 @@ conexion.commit()
 # Mostrar mensaje con el di generado
 print("Registros afectados",cursor.rowcount)
 print("Inserta con ID ",cursor.lastrowid)
+
+# Insert con multiples
+sentenciaInsert = "INSERT INTO empleados (nombre, apellido, departamento, puesto, salario) VALUES (%s,%s,%s,%s,%s)"
+datos = [
+    ("Carlos", "Alcantara", "Desarrollo", "Developer", "12345"),
+    ("Daniel", "Carrillo", "Desarrollo", "Developer", "12345")
+]
+
+# Executemany
+cursor.executemany(sentenciaInsert, datos)
+conexion.commit()
+
+# CRUD
+consultaSelect = "SELECT * FROM empleados"
+cursor.execute(consultaSelect)
+resultado = cursor.fetchall()
+for registro in resultado:
+    print(registro)
+"""
+print("SELECT con WHERE")
+consultaSelect = "SELECT * FROM empleados WHERE departamento = %s"
+# usar una tupla aunque sea 1 solo valor
+valor = ("Desarrollo",)
+cursor.execute(consultaSelect, valor)
+resultados = cursor.fetchall()
+for registro in resultados:
+    print(registro)
+
+print("DELETE")
+consultaDelete = "DELETE FROM empleados WHERE id = %s"
+valorDelete = (13,)
+cursor.execute(consultaDelete, valorDelete)
+conexion.commit()
+print("Registros borrados",cursor.rowcount)
+
+print("UPDATE")
+consultaUpdate = "UPDATE empleados SET departamento = %s WHERE id = %s"
+valorUpdate = ("Department",14)
+cursor.execute(consultaUpdate, valorUpdate)
+conexion.commit()
+print("Registros actualizados",cursor.rowcount)
+
+# Buena practica
+cursor.close()
